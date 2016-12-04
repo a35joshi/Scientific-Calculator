2a8b4f9e43d9882b8241f115e5e85496a9295ad6
Anurag Joshi <a35joshi@uwaterloo.ca>
2016-12-04 13:44:06 -0500

    Carrot Mark added.
    Anurag Joshi

--- app/src/main/java/scientificcalculatorapp/scientificcalculator/ScientificCalculator.java
+++ app/src/main/java/scientificcalculatorapp/scientificcalculator/ScientificCalculator.java
@@ -14,7 +14,7 @@ import net.objecthunter.exp4j.function.Function;
 
 
 public class ScientificCalculator extends AppCompatActivity {
-    int[] numberbuttons={R.id.button_SQR,R.id.button_SQR_ROOT,R.id.buttonSIN,R.id.buttonCOS,R.id.buttonTAN,R.id.button0,R.id.button1,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,R.id.button7,R.id.button8,R.id.button9,R.id.button_OPEN_BRACKET,R.id.button_CLOSE_BRACKET,R.id.button_COMMA};
+    int[] numberbuttons={R.id.button_CARROT_MARK,R.id.button_SQR,R.id.button_SQR_ROOT,R.id.buttonSIN,R.id.buttonCOS,R.id.buttonTAN,R.id.button0,R.id.button1,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,R.id.button7,R.id.button8,R.id.button9,R.id.button_OPEN_BRACKET,R.id.button_CLOSE_BRACKET,R.id.button_COMMA};
     int[] operatorbuttons={R.id.buttonplus,R.id.buttonminus,R.id.buttonmultiply,R.id.buttondivide};
     boolean buttonoperatorpressed = false;
     boolean trigopressed=false;
